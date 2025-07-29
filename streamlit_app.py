eec3f3838131f35e977dd7c73b4a947432cbd954
byegoner <jayfu2024@gmail.com>
2025-07-29 03:38:17 +0000

    button alignment

--- streamlit_app.py
+++ streamlit_app.py
@@ -437,7 +437,7 @@ if not sorter.is_done():
                           kwargs={"ship_name": ship["name"]})
 
     st.caption("none will eliminate ALL three ships, use sparingly!!")
-    my_grid = grid([.8, .1], 1, 2, vertical_align="bottom")
+    my_grid = grid([.8, .1], 1, 1, vertical_align="bottom")
     options = ["↺"]
     my_grid.pills("", options)
 
