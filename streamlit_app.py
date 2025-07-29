642412b9056cd677c43fe19c6c19a6721bdf171f
byegoner <jayfu2024@gmail.com>
2025-07-28 23:17:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,58 +1,65 @@
 import pandas as pd
 import random
 import streamlit as st
-from streamlit_extras.grid import grid
 import math
 
-members_1 = pd.Series(['Seoyeon', 'Jiwoo', 'Chaeyeon', 'Yooyeon', 'Nakyoung', 'Yubin', 'Kaede', 'Dahyun', 'Kotone', 'Nien', 'Sohyun', 'Xinyu', 'Mayu', 'Jiyeon'], index=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
-members_2 = pd.Series(['Seoyeon', 'Jiwoo', 'Chaeyeon', 'Yooyeon', 'Nakyoung', 'Yubin', 'Kaede', 'Dahyun', 'Kotone', 'Nien', 'Sohyun', 'Xinyu', 'Mayu', 'Jiyeon'], index=[0, 1, 2, 3, 4, 5, 6, 7, 8 ,9, 10, 11, 12, 13])
+members_1 = pd.Series(
+    ['Seoyeon', 'Jiwoo', 'Chaeyeon', 'Yooyeon', 'Nakyoung', 'Yubin', 'Kaede', 'Dahyun', 'Kotone', 'Nien', 'Sohyun',
+     'Xinyu', 'Mayu', 'Jiyeon'], index=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+members_2 = pd.Series(
+    ['Seoyeon', 'Jiwoo', 'Chaeyeon', 'Yooyeon', 'Nakyoung', 'Yubin', 'Kaede', 'Dahyun', 'Kotone', 'Nien', 'Sohyun',
+     'Xinyu', 'Mayu', 'Jiyeon'], index=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
 
 all_ships_init = []
 
-#Creating all possible pairing options
+# Creating all possible pairing options
 for i in range(len(members_1)):
     for j in range(len(members_2)):
         if j != i:
             if j > i:
-                all_ships_init += [(members_1[i]+","+members_2[j]).lower()]
+                all_ships_init += [(members_1[i] + "," + members_2[j]).lower()]
             elif j < i:
                 continue
         elif j == i:
             continue
 
+
 @st.cache_resource
 def get_ship_data():
     ships = {}
     for i in range(len(all_ships_init)):
-        ships[f"ship{i+1}"] = all_ships_init[i]
+        ships[f"ship{i + 1}"] = all_ships_init[i]
     return ships
 
+
 ships = get_ship_data()
+
 
 # Round and ranking functions adapted from:
 # https://github.com/pradigunara/sssongs/blob/b4c6eb3c0759f5d8f314a407181899b1c7285608/src/roundCalculator.js
 # https://github.com/pradigunara/sssongs/blob/b4c6eb3c0759f5d8f314a407181899b1c7285608/src/songSorter.js
 
 def calculate_rounds_1(ships_count):
-    total_rounds = (ships_count * (ships_count-1))/2
-    rounds_full_coverage = math.ceil(total_rounds/3)
-    min_appearances_ship = 4 #Makes sure that each option appears at least 4 times
-    total_appearances = ships_count*min_appearances_ship
-    rounds_appearances = math.ceil(total_appearances/3)
+    total_rounds = (ships_count * (ships_count - 1)) / 2
+    rounds_full_coverage = math.ceil(total_rounds / 3)
+    min_appearances_ship = 4  # Makes sure that each option appears at least 4 times
+    total_appearances = ships_count * min_appearances_ship
+    rounds_appearances = math.ceil(total_appearances / 3)
     base_rounds = max(rounds_full_coverage, rounds_appearances)
-    return math.ceil(base_rounds/5)*5
+    return math.ceil(base_rounds / 5) * 5
+
 
 def calculate_rounds_hybrid(total_ships):
-    min_appearances_ship = 3 #Makes sure that each option appears at least 3 times during the hybrid rounds
-    min_total_appearances = total_ships*min_appearances_ship
-    phase_1_rounds = math.ceil(min_total_appearances/3)
-    phase_1_rounds_rounded = math.ceil(phase_1_rounds/5)*5
-    phase_1_survivors = math.ceil(total_ships*0.5)
+    min_appearances_ship = 3  # Makes sure that each option appears at least 3 times during the hybrid rounds
+    min_total_appearances = total_ships * min_appearances_ship
+    phase_1_rounds = math.ceil(min_total_appearances / 3)
+    phase_1_rounds_rounded = math.ceil(phase_1_rounds / 5) * 5
+    phase_1_survivors = math.ceil(total_ships * 0.5)
     final_ships = 16
-    phase_2_rounds  = math.ceil((phase_1_survivors * 1.2)/3)
-    phase_2_rounds_rounded = math.ceil(phase_2_rounds/5)*5
+    phase_2_rounds = math.ceil((phase_1_survivors * 1.2) / 3)
+    phase_2_rounds_rounded = math.ceil(phase_2_rounds / 5) * 5
     phase_3_rounds = calculate_rounds_1(final_ships)
-    return{
+    return {
         "phase1": {
             "rounds": phase_1_rounds_rounded,
             "description": "phase 1",
@@ -73,6 +80,7 @@ def calculate_rounds_hybrid(total_ships):
         "totalRounds": phase_1_rounds_rounded + phase_2_rounds_rounded + phase_3_rounds,
         "phases": 3
     }
+
 
 def get_current_phase(current_round, config):
     p1 = config["phase1"]["rounds"]
@@ -103,6 +111,7 @@ def get_current_phase(current_round, config):
             "description": config["phase3"]["description"],
             "type": "head-to-head"
         }
+
 
 class shipsorter:
     def __init__(self, ships_dict):
@@ -212,166 +221,175 @@ class shipsorter:
     def is_done(self):
         return self.current_round >= self.total_rounds
 
-#Image paths
-ships_data= pd.Series(["./image/s1s3.webp","./image/s1s4.webp",
-"./image/s1s5.webp",
- "./image/s1s7.webp",
- "./image/s1s8.webp",
- "./image/s1s9.webp",
- "./image/s1s10wav_chan.jpeg",
- "./image/s1s11.jpeg",
- "./image/s1s13.webp",
- "./image/s1s14.webp",
- "./image/s1s15pumpkin030806.jpeg",
- "./image/s1s16.webp",
- "./image/s1s24.webp",
- "./image/s3s4.webp",
- "./image/s3s5.webp",
- "./image/s3s7.webp",
- "./image/s3s8.webp",
- "./image/s3s9chkchk.webp",
- "./image/s3s10.jpeg",
- "./image/s3s11topclass.jpeg",
- "./image/s3s13.jpeg",
- "./image/s3s14.jpeg",
- "./image/s3s15.png",
- "./image/s3s16ourl512o.jpeg",
- "./image/s3s24axharrrrr.jpeg",
- "./image/s4s5.jpeg",
- "./image/s4s7.webp",
- "./image/s4s8.webp",
- "./image/s4s9.jpeg",
- "./image/s4s10.jpeg",
- "./image/s4s11kotoneverdie.jpeg",
- "./image/s4s13.jpeg",
- "./image/s4s14.jpeg",
- "./image/s4s15.jpeg",
- "./image/s4s16.webp",
- "./image/s4s24.jpeg",
- "./image/s5s7.jpeg",
- "./image/s5s8.webp",
- "./image/s5s9kaede_acernus.jpeg",
- "./image/s5s10sodaruno.jpeg",
- "./image/s5s11.jpeg",
- "./image/s5s13a_honeynien.jpeg",
- "./image/s5s14.jpeg",
- "./image/s5s15mo4sheng1.jpeg",
- "./image/s5s16.jpeg",
- "./image/s5s24wavv57.jpeg",
- "./image/s7s8.webp",
- "./image/s7s9.webp",
- "./image/s7s10.webp",
- "./image/s7s11.jpeg",
- "./image/s7s13.webp",
- "./image/s7s14.jpeg",
- "./image/s7s15ssszxy.jpeg",
- "./image/s7s160sp346.jpeg",
- "./image/s7s24.jpeg",
- "./image/s8s9.jpeg",
- "./image/s8s10.jpeg",
- "./image/s8s11.jpeg",
- "./image/s8s13.webp",
- "./image/s8s14sohyunnabi14.jpeg",
- "./image/s8s15.jpeg",
- "./image/s8s16.webp",
- "./image/s8s24.jpeg",
- "./image/s9s10.jpeg",
- "./image/s9s11.webp",
- "./image/s9s13.jpeg",
- "./image/s9s14.jpeg",
- "./image/s9s15kconjapan.jpeg",
- "./image/s9s16.webp",
- "./image/s9s24kflow3onsite.jpeg",
- "./image/s10s11sunriseseoah.jpeg",
- "./image/s10s13.jpeg",
- "./image/s10s14atiu_atr.jpeg",
- "./image/s10s15wordofmyheart02.jpeg",
- "./image/s10s16.jpeg",
- "./image/s10s24refrain0213.png",
- "./image/s11s13.webp",
- "./image/s11s14.webp",
- "./image/s11s15-kttto11.jpeg",
- "./image/s11s16.webp",
- "./image/s11s24.webp",
- "./image/s13s14.jpg",
- "./image/s13s15.jpeg",
- "./image/s13s16wavchichi.jpeg",
- "./image/s13s240602_nien.jpeg",
- "./image/s14s15.jpg",
- "./image/s14s16.jpeg",
- "./image/s14s24.jpeg",
- "./image/s15s16.jpeg",
- "./image/s15s24.jpeg",
- "./image/s16s24.jpeg"], index=[all_ships_init])
 
-#If photo is not from official content (fansites mostly) source will be provided upon hovering the button
+# Image paths
+ships_data = pd.Series(["./image/s1s3.webp", "./image/s1s4.webp",
+                        "./image/s1s5.webp",
+                        "./image/s1s7.webp",
+                        "./image/s1s8.webp",
+                        "./image/s1s9.webp",
+                        "./image/s1s10wav_chan.jpeg",
+                        "./image/s1s11.jpeg",
+                        "./image/s1s13.webp",
+                        "./image/s1s14.webp",
+                        "./image/s1s15pumpkin030806.jpeg",
+                        "./image/s1s16.webp",
+                        "./image/s1s24.webp",
+                        "./image/s3s4.webp",
+                        "./image/s3s5.webp",
+                        "./image/s3s7.webp",
+                        "./image/s3s8.webp",
+                        "./image/s3s9chkchk.webp",
+                        "./image/s3s10.jpeg",
+                        "./image/s3s11topclass.jpeg",
+                        "./image/s3s13.jpeg",
+                        "./image/s3s14.jpeg",
+                        "./image/s3s15.png",
+                        "./image/s3s16ourl512o.jpeg",
+                        "./image/s3s24axharrrrr.jpeg",
+                        "./image/s4s5.jpeg",
+                        "./image/s4s7.webp",
+                        "./image/s4s8.webp",
+                        "./image/s4s9.jpeg",
+                        "./image/s4s10.jpeg",
+                        "./image/s4s11kotoneverdie.jpeg",
+                        "./image/s4s13.jpeg",
+                        "./image/s4s14.jpeg",
+                        "./image/s4s15.jpeg",
+                        "./image/s4s16.webp",
+                        "./image/s4s24.jpeg",
+                        "./image/s5s7.jpeg",
+                        "./image/s5s8.webp",
+                        "./image/s5s9kaede_acernus.jpeg",
+                        "./image/s5s10sodaruno.jpeg",
+                        "./image/s5s11.jpeg",
+                        "./image/s5s13a_honeynien.jpeg",
+                        "./image/s5s14.jpeg",
+                        "./image/s5s15mo4sheng1.jpeg",
+                        "./image/s5s16.jpeg",
+                        "./image/s5s24wavv57.jpeg",
+                        "./image/s7s8.webp",
+                        "./image/s7s9.webp",
+                        "./image/s7s10.webp",
+                        "./image/s7s11.jpeg",
+                        "./image/s7s13.webp",
+                        "./image/s7s14.jpeg",
+                        "./image/s7s15ssszxy.jpeg",
+                        "./image/s7s160sp346.jpeg",
+                        "./image/s7s24.jpeg",
+                        "./image/s8s9.jpeg",
+                        "./image/s8s10.jpeg",
+                        "./image/s8s11.jpeg",
+                        "./image/s8s13.webp",
+                        "./image/s8s14sohyunnabi14.jpeg",
+                        "./image/s8s15.jpeg",
+                        "./image/s8s16.webp",
+                        "./image/s8s24.jpeg",
+                        "./image/s9s10.jpeg",
+                        "./image/s9s11.webp",
+                        "./image/s9s13.jpeg",
+                        "./image/s9s14.jpeg",
+                        "./image/s9s15kconjapan.jpeg",
+                        "./image/s9s16.webp",
+                        "./image/s9s24kflow3onsite.jpeg",
+                        "./image/s10s11sunriseseoah.jpeg",
+                        "./image/s10s13.jpeg",
+                        "./image/s10s14atiu_atr.jpeg",
+                        "./image/s10s15wordofmyheart02.jpeg",
+                        "./image/s10s16.jpeg",
+                        "./image/s10s24refrain0213.png",
+                        "./image/s11s13.webp",
+                        "./image/s11s14.webp",
+                        "./image/s11s15-kttto11.jpeg",
+                        "./image/s11s16.webp",
+                        "./image/s11s24.webp",
+                        "./image/s13s14.jpg",
+                        "./image/s13s15.jpeg",
+                        "./image/s13s16wavchichi.jpeg",
+                        "./image/s13s240602_nien.jpeg",
+                        "./image/s14s15.jpg",
+                        "./image/s14s16.jpeg",
+                        "./image/s14s24.jpeg",
+                        "./image/s15s16.jpeg",
+                        "./image/s15s24.jpeg",
+                        "./image/s16s24.jpeg"], index=[all_ships_init])
+
+
+# If photo is not from official content (fansites mostly) source will be provided upon hovering the button
 def add_cap(x):
     if x == "seoyeon,dahyun":
-        return("wav_chan")
+        return ("wav_chan")
     elif x == "seoyeon,xinyu":
-        return("pumpkin030806")
+        return ("pumpkin030806")
     elif x == "jiwoo,kaede":
-        return("chkchk magazine")
+        return ("chkchk magazine")
     elif x == "jiwoo,kotone":
-        return("topclass magazine")
+        return ("topclass magazine")
     elif x == "jiwoo,mayu":
-        return("ourl512o")
+        return ("ourl512o")
     elif x == "jiwoo,jiyeon":
-        return("axharrrrr")
+        return ("axharrrrr")
     elif x == "chaeyeon,kotone":
-        return("kotoneverdie")
+        return ("kotoneverdie")
     elif x == "yooyeon,kaede":
-        return("kaede_acernus")
+        return ("kaede_acernus")
     elif x == "yooyeon,dahyun":
-        return("sodaruno")
+        return ("sodaruno")
     elif x == "yooyeon,nien":
-        return("a_honeynien")
+        return ("a_honeynien")
     elif x == "yooyeon,xinyu":
-        return("mo4sheng1")
+        return ("mo4sheng1")
     elif x == "yooyeon,jiyeon":
-        return("wavv57")
+        return ("wavv57")
     elif x == "nakyoung,xinyu":
-        return("ssszxy")
+        return ("ssszxy")
     elif x == "nakyoung,mayu":
-        return("0sp346")
+        return ("0sp346")
     elif x == "yubin,dahyun":
-        return("happyvirus_s8")
+        return ("happyvirus_s8")
     elif x == "yubin,sohyun":
-        return("sohyunnabi14")
+        return ("sohyunnabi14")
     elif x == "yubin,xinyu" or x == "dahyun,sohyun":
-        return("atiu_atr")
+        return ("atiu_atr")
     elif x == "kaede,xinyu":
-        return("kcon japan")
+        return ("kcon japan")
     elif x == "kaede,jiyeon":
-        return("kflow3 onsite")
+        return ("kflow3 onsite")
     elif x == "dahyun,kotone":
-        return("sunriseseoah")
+        return ("sunriseseoah")
     elif x == "dahyun,xinyu":
-        return("wordofmyheart02")
+        return ("wordofmyheart02")
     elif x == "dahyun,mayu":
-        return("nme the cover")
+        return ("nme the cover")
     elif x == "dahyun,jiyeon":
-        return("refrain0213")
+        return ("refrain0213")
     elif x == "kotone,xinyu":
-        return("kttto11")
+        return ("kttto11")
     elif x == "nien,mayu":
-        return("wavchichi")
+        return ("wavchichi")
     elif x == "nien,jiyeon":
-        return("0602_nien")
+        return ("0602_nien")
     else:
-        return("")
+        return ("")
+
 
 if "sorter" not in st.session_state:
     st.session_state.sorter = shipsorter(ships)
 
+if "eliminated" not in st.session_state:
+    st.session_state.eliminated = False
+
 if "selected" not in st.session_state:
     st.session_state.selected = False
+
 
 def eliminate_current_ships():
     sorter.eliminate_ships([ship["name"] for ship in sorter.current_options])
 
+
 def selected_click(ship_name):
     st.session_state.selected = ship_name
+
 
 sorter = st.session_state.sorter
 
@@ -380,29 +398,28 @@ st.markdown(
     ":blue-badge[05z+ only] :blue-badge[better on desktop] :blue-badge[adapted from [@celdaris](https://x.com/celdaris)]"
 )
 
-#Mobile-friendly checkbox
+# Mobile-friendly checkbox
 on = st.checkbox("images on/off (mobile)", value=True)
 
-#Actual app/executions of functions
+# Actual app/executions of functions
 if not sorter.is_done():
-    
     if st.session_state.selected:
         selected_ship = st.session_state.selected
         sorter.record_winner(selected_ship)
         st.session_state.selected = False
 
     phase_info = sorter.get_current_phase_info()
-    #st.write(sorter.total_rounds) <-- Uncomment to check how adjustments to the ranking system impact round number
+    # st.write(sorter.total_rounds) <-- Uncomment to check how adjustments to the ranking system impact round number
 
-    #Progress bar
+    # Progress bar
     progress = (sorter.current_round + 1) / sorter.total_rounds
-    #st.caption(f"round {sorter.current_round}")
+    # st.caption(f"round {sorter.current_round}")
     if sorter.current_round < sorter.total_rounds:
         st.progress(progress)
-    elif sorter.current_round-1 >= sorter.total_rounds:
+    elif sorter.current_round - 1 >= sorter.total_rounds:
         ""
 
-    #Grabbing three pairings and populating image/button options into 3 containers
+    # Grabbing three pairings and populating image/button options into 3 containers
     current_ships = sorter.select_three_ships()
     cols = st.columns(3)
 
@@ -418,19 +435,24 @@ if not sorter.is_done():
                           on_click=selected_click,
                           kwargs={"ship_name": ship["name"]})
 
-    st.caption("none will eliminate ALL three ships, use sparingly!!")
-    my_grid = grid([.8, .1], 2, vertical_align="bottom")
-    options = ["↺"]
-    my_grid.pills("", options)
-    my_grid.button("none")
+    st.caption("for best results shuffle for neither/none")
 
-#Showing top 10 rankings with image attachment for number one
+    st.button("none", on_click=eliminate_current_ships)
+
+    # Shuffle button
+    # col1, col2, col3 = st.columns([1, 1, 2.32])
+    # with col3:
+    # options = ["↺"]
+    # if st.pills("", options):
+    # current_ships = sorter.select_three_ships()
+
+# Showing top 10 rankings with image attachment for number one
 else:
-    st.subheader("top 10", divider = "blue")
+    st.subheader("top 10", divider="blue")
     rankings = sorter.get_rankings()
     for idx, ship in enumerate(rankings[:10], 1):
-        if idx <2:
+        if idx < 2:
             col1, col2, col3 = st.columns([1, 1, 1])
             with col2:
-                st.image(ships_data[ship['name']], width = 200, caption = add_cap(ship["name"]))
+                st.image(ships_data[ship['name']], width=200, caption=add_cap(ship["name"]))
         st.write(f"{idx}. {ship['name'].replace(',', ' ♡ ')}")
